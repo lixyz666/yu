5c0358ed252b78046785d3111d2cd432765516a2
lixyz666 <97370636+lixyz666@users.noreply.github.com>
2023-08-25 15:53:17 +0800

    index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -104,9 +104,10 @@ $(function () {
         // 旗下站点
         const xf_site = {
             siteInfo: [
+                 { name: 'ChatGPT', links: 'https://chat.lixuanzer.top/', pic: 'assets/upload/chatgpt.jpg' },
                 { name: '李眩しい沢の博客', links: 'https://blog.lixuanzer.top/', pic: 'assets/upload/blog.jpg' },
                 { name: '我的个人主页', links: 'https://my.lixuanzer.top/', pic: 'assets/upload/grzy.png' },
-                { name: '中转页', links: 'https://www.lixuanzer.top/', pic: 'assets/upload/zhongzhuan.jpg' },
+                { name: '中转页', links: 'https://www.lixuanzer.top/', pic: 'assets/upload/zhongzhuan.png' },
             ]
         }
 
